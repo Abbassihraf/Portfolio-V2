9977c76c0ab4cf6ee38c697267cc25ed89cef732
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-13 01:30:32 +0100

    Compile scss into css function

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,25 @@ const fileinclude = require('gulp-file-include');
 const imagemin = require('gulp-imagemin');
 const cache = require('gulp-cache');
 const terser = require('gulp-terser');
+
+
+
+//compile scss into css
+let style = () => {
+    // 1. where is my scss file
+    return gulp.src('src/sass/**/*.scss')
+        // 2. pass the file through sass compiler
+        .pipe(sass().on('error', sass.logError))
+        // 3. pass the file through autoprifixer
+        .pipe(autoprefixer())
+        // 4. pass the file through css minifier
+        .pipe(cssnano())
+        // 5. intilizing sourcemaps
+        .pipe(sourcemaps.init())
+        // 6. pass the files through sourcemaps ?
+        .pipe(sourcemaps.write('.'))
+        // 7. where do i save the compiled CSS ?
+        .pipe(gulp.dest('dist/css'))
+        // 8. stream changes to all browsers
+        .pipe(browserSync.stream());
+}
