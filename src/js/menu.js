3879b73cb5d626830795a15a24f6eb32d182c8e4
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-23 00:40:49 +0100

    Fix navbar problem
    Fix navbar for all VERSIONS & make it fixeed too when I scroll

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,49 +3,49 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
-// const linksContainer = document.querySelector('.links-container');
-// const links = document.querySelector('.links');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 // console.log(links);
-// const navToggle = document.querySelector('.nav-toggle i');
+const navToggle = document.querySelector('.nav-toggle i');
 
-// navToggle.addEventListener('click',() =>{
+navToggle.addEventListener('click',() =>{
 
-//     // linksContainer.classList.toggle('show-links');
-//     const containerHeight = linksContainer.getBoundingClientRect().height;
-//     // console.log(containerHeight);
-//     const linksHeight = links.getBoundingClientRect().height;
-//     // console.log(linksHeight);
-//     if(containerHeight === 0)
-//     {
-//         linksContainer.style.height = `${linksHeight}px`;
-//     }
-//     else{
-//         linksContainer.style.height = 0;
-//     }
+    // linksContainer.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    // console.log(containerHeight);
+    const linksHeight = links.getBoundingClientRect().height;
+    // console.log(linksHeight);
+    if(containerHeight === 0)
+    {
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+    else{
+        linksContainer.style.height = 0;
+    }
   
 
-// });
+});
 
 // ********** fixed navbar ************
-// const navbar = document.getElementById('nav');
-// const topLink = document.querySelector('.top-link'); 
-// window.addEventListener('scroll',()=>{
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link'); 
+window.addEventListener('scroll',()=>{
 
-    // console.log(window.pageYOffset);
+    console.log(window.pageYOffset);
 
-//     const scrollHeight = window.pageYOffset;
-//     const navHeight = navbar.getBoundingClientRect().height;
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
 
-//     if(scrollHeight > navHeight)
-//     {
-//         navbar.classList.add('fixed-nav');
-//     }
+    if(scrollHeight > navHeight)
+    {
+        navbar.classList.add('fixed-nav');
+    }
 
-//     else{
+    else{
 
-//         navbar.classList.remove('fixed-nav');
-//     }
-// });
+        navbar.classList.remove('fixed-nav');
+    }
+});
 
 
 
