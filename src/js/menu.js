567fbb8f413356b272868343b06bcc59c78dd00d
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-24 00:33:08 +0100

    Include link copyright.js into footer.html

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,13 @@
-
-// ********** set date ************
-const date = document.getElementById('date');
-date.innerHTML = new Date().getFullYear();
 // ********** close links ************
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
-// console.log(links);
 const navToggle = document.querySelector('.nav-toggle i');
 
 navToggle.addEventListener('click',() =>{
 
-    // linksContainer.classList.toggle('show-links');
     const containerHeight = linksContainer.getBoundingClientRect().height;
-    // console.log(containerHeight);
     const linksHeight = links.getBoundingClientRect().height;
-    // console.log(linksHeight);
+
     if(containerHeight === 0)
     {
         linksContainer.style.height = `${linksHeight}px`;
