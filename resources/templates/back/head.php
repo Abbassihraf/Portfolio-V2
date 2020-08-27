96c554a1457467bc7af197853d5ffe9e42cadaa9
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-27 03:33:39 +0100

    Restructuring base files & workflow #15

--- resources/templates/back/head.php
+++ resources/templates/back/head.php
@@ -7,7 +7,7 @@
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="../assets/images/icon-onglet.png">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>2AI dashboard</title>
+    <title>Portfolio AA | dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
     <meta name="msapplication-tap-highlight" content="no">
