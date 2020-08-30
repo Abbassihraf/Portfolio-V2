3a2e6ae0c3748aea293362dba0e8cfd7c9de5cf2
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-31 00:45:48 +0100

    Create about-section.php

---
+++ resources/templates/front/about-section.php
@@ -0,0 +1,86 @@
+<div class="A-s-s" id="about">
+        <div><img class="img9" src="assets/images/dots.png" alt=""></div>
+        <div><img class="img10" src="assets/images/dots-vertical.png" alt=""></div>
+        <div><img class="img11" src="assets/images/line-vertical.png" alt=""></div>
+        
+<div class="aboutMe__list">
+    <button class="aboutMe__aboutBtn activeBtn">about</button>
+    <button class="aboutMe__serviceBtn">service</button>
+    <button class="aboutMe__skillsBtn">skills</button>
+</div>
+<section class="aboutMe">
+    <div class="aboutMe__wrapper">
+        <div class="aboutMe__wrapper--left">
+            <img src="assets/images/about-pic.png" alt="" data-aos="flip-left" data-aos-duration="1200">
+        </div>
+        <div class="aboutMe__wrapper--right">
+            <h1>about me</h1>
+            <h2>Why hire me for your next project?</h2>
+            <h3>
+                I am a web developer, ambitious person who has developed a mature and responsible approach to any task I undertake, or situation that I am presented with. As a graduate with one year of experience in web development. I am excellent at working with others to achieve a certain objective on time and with excellence.
+            </h3>
+            <a href="" class="Download--cv">Download CV</a>
+        </div>
+    </div>
+</section>
+
+<section class="service">
+    <div class="service__wrapper">
+        <div class="service__card" data-aos="flip-right" data-aos-duration="1200">
+            <img src="assets/images/design-icon.png" alt="design">
+            <h1 class="service__card--title">Design</h1>
+            <p class="service__card--description">Having a unique website
+                design is an important part
+                of your company's branding
+                and marketing process.</p>
+        </div>
+        <div class="service__card" data-aos="flip-right" data-aos-duration="1200">
+            <img src="assets/images/dev-icon.png" alt="devlopement">
+            <h1 class="service__card--title">Development</h1>
+            <p class="service__card--description">We build websites and web
+                applications using the best
+                technologies that work hard,
+                achieve results, and generate
+                a return on investment.</p>
+        </div>
+        <div class="service__card" data-aos="flip-right" data-aos-duration="1200">
+            <img src="assets/images/mobile-icon.png" alt="mobile design & developement">
+            <h1 class="service__card--title">Mobile-Friendly</h1>
+            <p class="service__card--description">A responsive design makes
+                your website accessible to
+                all users, regardless of their
+                device.</p>
+        </div>
+    </div>
+</section>
+
+<section class="skills">
+    <div class="skills__wrapper">
+        <div class="skills__card" data-aos="flip-down" data-aos-duration="1200">
+            <img src="assets/images/leader.png" alt="Leadership">
+            <h1 class="skills__title">leadership</h1>
+        </div>
+        <div class="skills__card" data-aos="flip-down" data-aos-duration="1200">
+                <img src="assets/images/teamwork.png" alt="Teamwork">
+                <h1 class="skills__title">teamwork</h1>
+        </div>
+        <div class="skills__card" data-aos="flip-down" data-aos-duration="1200">
+                <img src="assets/images/organization.png" alt="organization">
+                <h1 class="skills__title">organization</h1>
+        </div>
+        <div class="skills__card" data-aos="flip-down" data-aos-duration="1200">
+                    <img src="assets/images/team-man.png" alt="Team management">
+                    <h1 class="skills__title">team management</h1>
+        </div>
+        <div class="skills__card" data-aos="flip-down" data-aos-duration="1200">
+                <img src="assets/images/self.png" alt="Self-learning">
+                <h1 class="skills__title">self-learning</h1>
+        </div>
+        <div class="skills__card" data-aos="flip-down" data-aos-duration="1200">
+                    <img src="assets/images/strategy.png" alt="Project management">
+                    <h1 class="skills__title">Project management</h1>
+        </div>
+    </div>
+</section>
+
+</div>
