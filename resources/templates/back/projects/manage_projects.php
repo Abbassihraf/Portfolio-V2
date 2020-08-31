d5ba9aae417fbe78126455e7fa399a0638bf7f43
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-31 11:28:08 +0100

    Create manage_projects.php

--- resources/templates/back/projects/manage_projects.php
+++ resources/templates/back/projects/manage_projects.php
@@ -1,4 +1,4 @@
-<?php delete_testimonials(); ?>
+
 <div class="app-main__inner">
     <div class="app-page-title">
         <div class="page-title-wrapper">
@@ -7,8 +7,8 @@
                     <i class="pe-7s-car icon-gradient bg-mean-fruit">
                     </i>
                 </div>
-                <div>Manage your recommandations
-                    <div class="page-title-subheading">Here you can modify your recommandations informations
+                <div>Manage your projects
+                    <div class="page-title-subheading">Here you can modify your projects informations
                     </div>
                 </div>
             </div>
@@ -17,21 +17,20 @@
     <div class="row">
         <div class="col-md-12">
             <div class="main-card mb-3 card">
-                <div class="card-header">client list</div>
+                <div class="card-header">Projects list</div>
                 <div class="table-responsive">
                     <table class="align-middle mb-0 table table-borderless table-striped table-hover">
                         <thead>
                             <tr>
                                 <th class="text-center">#</th>
-                                <th>thumbnail</th>
-                                <th>Name</th>
-                                <th>Last Name</th>
-                                <th>Role</th>
-                                <th>Content</th>
+                                <th>Thumbnail</th>
+                                <th>Title</th>
+                                <th>Github</th>
+                                <th>Website link</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php display_testimonials_admin() ?>
+                            <?php display_prjects_admin() ?>
                         </tbody>
                     </table>
                 </div>
