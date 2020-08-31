fa198ba7511dd98a94bdf5e1fd858847095c2d5f
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-31 01:26:10 +0100

    Functionality for displaying testimonials to be edited or deleted in admin area

--- public/admin/index.php
+++ public/admin/index.php
@@ -21,6 +21,9 @@ include(TEMPLATE_BACK . DS . "head.php");
         if(isset($_GET['create_testimonials'])){
             include(TEMPLATE_BACK . DS . "testimonials/create_testimonials.php");
         }
+        if(isset($_GET['manage_testimonials'])){
+            include(TEMPLATE_BACK . DS . "testimonials/manage_testimonials.php");
+        }
 
 
 
--- resources/component/testimonialsComponent.php
+++ resources/component/testimonialsComponent.php
@@ -61,7 +61,41 @@ function submit_testimonials(){
     }
 }
 
+// Testimnials Management. display testimonials to be edited or deleted in admin area
 
+function display_testimonials_admin()
+{
+    global $pdo;
+    try{
+        $sql = "SELECT t.*, m.file_name FROM testimonials t join media m on t.profile = m.id "; 
+        $stmt = $pdo->query($sql)->fetchAll();
+        foreach ($stmt as $testimonials){
+        echo <<<testimonials
+        <tr>
+        <td class="text-center text-muted">{$testimonials->id}</td>
+        <td class=""><img src="../uploads/thumbnails/{$testimonials->file_name}" class="br-a" alt="testimonials thumbnail"></td>
+        <td class=""> {$testimonials->name} </td>
+        <td class=""> {$testimonials->last_name} </td>
+        <td class=""> {$testimonials->role} </td>
+        <td class=""> {$testimonials->content} </td>
+
+        <td class="text-center">
+            <a href="index.php?edit_testimonials={$testimonials->id}">
+            <button type="button" id="PopoverCustomT-1"class=" btn-wide btn btn-success btn-icon-only">
+                <i class="pe-7s-note" style="font-size: 1rem;"></i> Edit
+            </button>
+            </a>
+            <button type="button" id="PopoverCustomT-1" class=" btn-icon btn-icon-only btn btn-outline-danger" value="index.php?manage_testimonials&delete_testimonials={$testimonials->id}" data-toggle="modal" data-target="#exampleModal">
+                <i class="pe-7s-trash" style="font-size: 1rem;"></i>
+            </button>
+        </td>
+    </tr>
+testimonials;
+    }
+} catch (PDOException $e) {
+    echo 'query failed' . $e->getMessage();
+}
+}
 
 
 
