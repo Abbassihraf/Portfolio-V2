d955063c0618763a8321c80e261c762165c89acf
Abbassihraf <hraf.abbassi@gmail.com>
2020-08-31 01:57:33 +0100

    Functionality for displaying 4 last projects from DB order by time into home page

--- resources/component/projectsComponenet.php
+++ resources/component/projectsComponenet.php
@@ -1,5 +1,6 @@
 <?php 
 
+//Functionality for displaying projects from DB order by time into projects page
 
 function display_projects(){
     global $pdo;
@@ -9,15 +10,15 @@ function display_projects(){
     foreach ($stmt as $projects){
         echo <<<projects
 
-        <div class="projects-page__card">
+        <div class="projects-page__card" data-aos="flip-left" data-aos-duration="1200">
         <div class="projects-page__card--top">
             <img src="uploads/{$projects->file_name}" alt="{$projects->title}"></div>
         <div class="projects-page__card--bottom">
-            <a href="{$projects->link}">
+            <a href="{$projects->link}" target="_blank">
                 <h1>{$projects->title}</h1>
             </a>
             <h2>GitHub:</h2>
-            <a class="projects-page__github" href="{$projects->github}" target="_blank" >https://github.com/Abbassihraf/P-curiosity-LAB</a>
+            <a class="projects-page__github" href="{$projects->github}" target="_blank" >{$projects->github}</a>
             <a class="projects-page__website" href="{$projects->link}" target="_blank"">website link</a>
         </div>
     </div>
@@ -29,7 +30,35 @@ projects;
     }
 }
 
+//Functionality for displaying 4 last projects from DB order by time into home page
 
+function display_last_projects(){
+    global $pdo;
+    try{
+    $sql ="SELECT p.*, m.file_name FROM projects p join media m on  p.cover = m.id ORDER BY p.id DESC LIMIT 4";
+    $stmt = $pdo->query($sql)->fetchAll();
+    foreach ($stmt as $projects){
+        echo <<<projects
+
+        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
+        <div class="projects__card--top">
+            <img class="coverProjects" src="uploads/{$projects->file_name}" alt="{$projects->title}">
+        </div>
+        <div class="projects__card--bottom">
+            <a href="{$projects->link}"><h1>{$projects->title}</h1></a>
+            <h2>GitHub:</h2>
+            <a class="projects__github" href="{$projects->github}" target="_blank" >{$projects->github}</a>
+            <a class="projects__website" href="{$projects->link}" target="_blank" >website link</a>
+        </div>
+    </div>
+
+projects;
+    }
+    } catch (PDOException $e) {
+    set_message('error','query failed');
+    echo 'query failed' . $e->getMessage();
+    }
+}
 
 
 
--- resources/templates/front/projects-section.php
+++ resources/templates/front/projects-section.php
@@ -11,50 +11,8 @@
             "991": {"items": 2, "margin": 40},
             "1449": {"items": 3, "margin": 24}
         } }'>
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-            <div class="projects__card--top">
-                <img class="coverProjects" src="images/project-cover1.png" alt="">
-            </div>
-            <div class="projects__card--bottom">
-                <a href=""><h1>p-curiosity lab website</h1></a>
-                <h2>GitHub:</h2>
-                <a class="projects__github" href="">https://github.com/Abbassihraf/P-curiosity-LAB</a>
-                <a class="projects__website" href="">website link</a>
-            </div>
-        </div>
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-                <div class="projects__card--top">
-                    <img class="coverProjects" src="images/project-cover2.png" alt="">
-                </div>
-                <div class="projects__card--bottom">
-                        <a href=""><h1>Ibtikar Arab Agency Website</h1></a>
-                    <h2>GitHub:</h2>
-                    <a class="projects__github" href="">https://github.com/Abbassihraf/P-curiosity-LAB</a>
-                    <a class="projects__website" href="">website link</a>
-                </div>
-        </div>
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-                <div class="projects__card--top">
-                    <img class="coverProjects" src="images/project-cover3.png" alt="">
-                </div>
-                <div class="projects__card--bottom">
-                        <a href=""><h1>Covid Test Website</h1></a>
-                    <h2>GitHub:</h2>
-                    <a class="projects__github" href="">https://github.com/Abbassihraf/P-curiosity-LAB</a>
-                    <a class="projects__website" href="">website link</a>
-                </div>
-        </div>
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-                    <div class="projects__card--top">
-                        <img class="coverProjects" src="images/project-cover4.png" alt="">
-                    </div>
-                    <div class="projects__card--bottom">
-                            <a href=""><h1>Youweb Agency Website</h1></a>
-                        <h2>GitHub:</h2>
-                        <a class="projects__github" href="">https://github.com/Abbassihraf/P-curiosity-LAB</a>
-                        <a class="projects__website" href="">website link</a>
-                    </div>
-        </div>
+
+        <?php display_last_projects() ?>
     </div>
     <a href="projects.php" class="projects__more">see all</a>
     </div>
