1b9fc4e6b23685b5a87b6e130c4e3f8aa9c08fc8
Abbassihraf <hraf.abbassi@gmail.com>
2020-09-05 00:26:53 +0100

    Edit login

--- public/admin/index.php
+++ public/admin/index.php
@@ -12,6 +12,8 @@ include(TEMPLATE_BACK . DS . "head.php");
     <div class="app-main__outer">
 
         <?php 
+        $access_admin = 1;
+
         if($_SERVER['REQUEST_URI'] == "/admin/" || $_SERVER['REQUEST_URI'] == "/admin/index.php?loginSuccess" || $_SERVER['REQUEST_URI'] == "/admin/index.php"){
             include(TEMPLATE_BACK . DS . "dashboard.php") ;
         }
@@ -19,34 +21,34 @@ include(TEMPLATE_BACK . DS . "head.php");
         // Testimonials requests
         
         if(isset($_GET['create_testimonials'])){
-            include(TEMPLATE_BACK . DS . "testimonials/create_testimonials.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "testimonials/create_testimonials.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
         if(isset($_GET['manage_testimonials'])){
-            include(TEMPLATE_BACK . DS . "testimonials/manage_testimonials.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "testimonials/manage_testimonials.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
         if(isset($_GET['edit_testimonials'])){
-            include(TEMPLATE_BACK . DS . "testimonials/edit_testimonials.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "testimonials/edit_testimonials.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
 
         // Projects requests
         
         if(isset($_GET['create_projects'])){
-            include(TEMPLATE_BACK . DS . "projects/create_projects.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "projects/create_projects.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
         if(isset($_GET['manage_projects'])){
-            include(TEMPLATE_BACK . DS . "projects/manage_projects.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "projects/manage_projects.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
         if(isset($_GET['edit_projects'])){
-            include(TEMPLATE_BACK . DS . "projects/edit_projects.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "projects/edit_projects.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
 
-        // Projects requests
+        // Curriculm requests
         
         if(isset($_GET['create_cv'])){
-            include(TEMPLATE_BACK . DS . "curriculum/create_cv.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "curriculum/create_cv.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
         if(isset($_GET['manage_cv'])){
-            include(TEMPLATE_BACK . DS . "curriculum/manage_cv.php");
+            $_SESSION['access'] == $access_admin ?  include(TEMPLATE_BACK . DS . "curriculum/manage_cv.php"):  include(TEMPLATE_BACK . DS . "notAuth.php") ;
         }
 
 
