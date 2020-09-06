f28c0784dbc653eb4c5ab489590516e32c8c066a
Abbassihraf <hraf.abbassi@gmail.com>
2020-09-06 12:35:20 +0100

    Add Error page & change user profile

---
+++ public/404.php
@@ -0,0 +1,14 @@
+@@include('includes/head.html')
+
+<section class="error">
+    <div class="error__container">
+        <div class="error__left">
+            <h1>error</h1>
+            <h2>Oops! Page Not Found.</h2>
+            <a href="index.php">go home</a>
+        </div>
+        <div class="error__right">
+            <img src="assets/images/error-image.png" alt="404 error">
+        </div>
+    </div>
+</section>
--- public/info.php
+++ public/info.php
@@ -1,1 +1,6 @@
 <?php phpinfo() ?>
+avatar.jpg
+jpg
+26.152KB
+../uploads/avatar.jpg
+image
